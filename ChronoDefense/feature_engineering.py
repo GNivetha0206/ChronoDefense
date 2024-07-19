6a10c2ef3cac5706ab5260f14ec104fcf2a9c542
NIVETHA G <166518319+GitNivetha@users.noreply.github.com>
2024-07-19 10:42:23 +0530

    Delete ChronoDefense directory

--- ChronoDefense/feature_engineering.py
+++
@@ -1,4 +0,0 @@
-def feature_engineering(data):
-    features = data[['attack_type', 'affected_system', 'attack_vector', 'timestamp']]
-    labels = data['label']  # Assuming there's a 'label' column for attack types or severity
-    return features, labels
--- ChronoDefense/visualization.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def visualize_data(data):
-    data['attack_type'].value_counts().plot(kind='bar')
-    plt.xlabel('Attack Type')
-    plt.ylabel('Frequency')
-    plt.title('Distribution of Attack Types')
-    plt.show()
